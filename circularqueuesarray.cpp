152adf9ee543f70947bcf52fb388cd470e2b09f2
canzonadzka <canzonadzka@gmail.com>
2026-05-28 15:00:31 +0700

    mengcek apakah antrian penuh

--- circularqueuesarray.cpp
+++ circularqueuesarray.cpp
@@ -21,4 +21,8 @@ class Queues
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
+
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
  
